18e16937498b9759b1076514354dab9ea5692c8a
Diana Clabel <dianaclabel08@gmail.com>
2024-01-30 13:28:47 -0500

    Se hace uso de useState

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 // import React from "react";
 // Despues de la version 17 de react ya no es necesario estar importando react a cada uno.
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  console.log(categories);
   return (
     <div>
       <>
@@ -9,6 +13,12 @@ export const GifExpertApp = () => {
         <h1>GifExpertApp</h1>
         {/* input */}
         {/* listado de gif */}
+        <ol>
+          {categories.map((category) => {
+            return <li key={category}>{category}</li>;
+          })}
+          {/* <li>ABC</li> */}
+        </ol>
         {/* gif item */}
       </>
     </div>
