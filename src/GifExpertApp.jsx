d4680202f2a4284dd5ee6c046f9cf14ad0b609ef
Diana Clabel <dianaclabel08@gmail.com>
2024-01-29 23:07:41 -0500

    Se relaiza el componente principal

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// Despues de la version 17 de react ya no es necesario estar importando react a cada uno.
+
+export const GifExpertApp = () => {
+  return (
+    <div>
+      <>
+        {/* titulo */}
+        <h1>GifExpertApp</h1>
+        {/* input */}
+        {/* listado de gif */}
+        {/* gif item */}
+      </>
+    </div>
+  );
+};
