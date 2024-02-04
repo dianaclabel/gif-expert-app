a6acd7208051aecf806c9184d9e8cd32b1a42676
Diana Clabel <dianaclabel08@gmail.com>
2024-02-03 23:43:34 -0500

    Se realiza una peticion a Gifhy

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,20 @@
 export const GifGrid = ({ category }) => {
+  const getGift = async () => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=SqRM3ZfhIjlTAYZgWz2WYbGSVIKhqE7P&q=${category}&limit=20`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map((img) => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    }));
+
+    console.log(gifs);
+  };
+
+  getGift();
+
   return (
     <>
       <h3>{category}</h3>
