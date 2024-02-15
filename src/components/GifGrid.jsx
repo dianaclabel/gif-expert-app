7a5bb5e4559652c3aaad91e23ffeb60475dfdf49
Diana Clabel <dianaclabel08@gmail.com>
2024-02-15 14:07:48 -0500

    Se agregael elemento de carga de imagenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -11,6 +11,8 @@ export const GifGrid = ({ category }) => {
     <>
       <h3>{category}</h3>
 
+      {isLoading && <h2>cargando...</h2>}
+
       <div className="card-grid">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
