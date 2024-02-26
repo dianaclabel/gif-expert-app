689bd081c58b8e79885e37de840a540cefb90569
Diana Clabel <dianaclabel08@gmail.com>
2024-02-26 18:46:47 -0500

    Se realiza test con jest.fn a onNewCategory

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -14,9 +14,10 @@ describe("Pruebas en <AddCategory/>", () => {
 
   test("Debe de llamar onNewCategory si el input tiene un valor", () => {
     const inputValue = "Saitama";
+    const onNewCategory = jest.fn();
     // TODO:???
 
-    render(<AddCategory onNewCategory={() => {}} />);
+    render(<AddCategory onNewCategory={onNewCategory} />);
 
     const input = screen.getByRole("textbox");
     // Recuerda colocar un area-label para que testing-librery pueda saber que tenemos ese label
@@ -27,6 +28,22 @@ describe("Pruebas en <AddCategory/>", () => {
     fireEvent.submit(form);
     // screen.debug();
 
-    expect(input.value).toBe("");
+    expect(input.value).toBe(" ");
+
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+
+  test("No debe llamar el onNewCategory si el input esta vacio", () => {
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    const form = screen.getByRole("form");
+    fireEvent.submit(form);
+
+    expect(onNewCategory).toHaveBeenCalledTimes(0);
+    expect(onNewCategory).not.toHaveBeenCalled();
   });
 });
